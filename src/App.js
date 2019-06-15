386060d8e0e399228d431a766e9bd414b7c400ed
app <app@siesgst.local>
2019-06-15 15:17:32 +0530

    neem ka patta

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      username:"",
+      password:"",
+      isLoggedIn:false
+    }
+  }
+
+  onChgtxt=(e)=>{
+    this.setState({
+      username:e.target.value
+      
+    })
+    console.log(e.target.value)
+  }
+  onPswdTxtChg=(e)=>{
+    this.setState({
+      password:e.target.value
+      
+    })
+    console.log(e.target.value)
+  }
+
+  
+  Onclick=(e)=>{
+    const{username,password}=this.state;
+    fetch('https://cors-anywhere.herokuapp.com/https://us-central1-plusbeat-a5425.cloudfunctions.net/submitResponse',
+    {   
+       method:'POST',
+       headers:{
+         'Content-Type' : 'application/json'
+        },
+      body:JSON.stringify({
+          email:username,
+          password:password
+       })
+    })
+    
+    .then(res => res.json())
+    .then((data) => { 
+      if(data.status==="success")
+      { this.setState({
+        isLoggedIn:true
+      })
+      }
+      console.log(data)
+    })
+    .catch(error => console.log(error))
+  }
+
+  render() {
+    const {password,username,isLoggedIn}=this.state
+   if(isLoggedIn){
+     return(
+       <div><h1>HOLY COW!</h1></div>
+     )
+   }
+   else
+   {
+    return(
+
+      <div>
+      <input onChange={this.onChgtxt} placeholder={"naam"} value={username} ></input>
+      <input onChange={this.onPswdTxtChg} type={"password"} placeholder={"pawswad"} value={password}></input>
+      <button onClick={this.Onclick}>Low budget Button</button>  
+      </div>
+ 
+    )
+    
+  }
+
+}
+}
 export default App;
